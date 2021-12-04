5482d248346b1a48189bdf0c2e771f24eccb1df6
Jared <TheWolfBadger@gmail.com>
2021-12-04 12:33:22 -0500

    day1 and day2 solutions all done (parts 1 and 2)

--- day1.php
+++ day1.php
@@ -3,14 +3,34 @@
  * Author: Jared Scarito
  * Created: 12/3/2021 12:47 PM
  * /**/
-$input = fopen(__DIR__ . "/inputs/day1.txt", 'r') or die("Unable to open file...");
-$previousLine = null;
-$increaseCount = 0;
-while ( ($line = fgets($input, 2024)) !== false) {
-    if ($previousLine != null)
-        if (intval($previousLine) < intval($line))
-            $increaseCount++;
-    $previousLine = $line;
+function part1() {
+    $input = fopen(__DIR__ . "/inputs/day1.txt", 'r') or die("Unable to open file...");
+    $previousLine = null;
+    $increaseCount = 0;
+    while (($line = fgets($input, 2024)) !== false) {
+        if ($previousLine != null)
+            if (intval($previousLine) < intval($line))
+                $increaseCount++;
+        $previousLine = $line;
+    }
+    fclose($input);
+    echo "$increaseCount";
 }
-fclose($input);
-echo "$increaseCount";
+function part2() {
+    $input = file_get_contents(__DIR__ . "/inputs/day1.txt", 'r') or die("Unable to open file...");
+    $panes = [];
+    $lines = explode("\n", $input);
+    for ($i = 0; $i < (sizeof($lines) - 2); $i++) {
+        $measure1 = $lines[$i];
+        $measure2 = $lines[$i + 1];
+        $measure3 = $lines[$i + 2];
+        $sum = $measure1 + $measure2 + $measure3;
+        $panes[$i] = $sum;
+    }
+    $increaseCount = 0;
+    for ($i = 0; $i < (sizeof($panes) - 1); $i++) {
+        if ($panes[$i + 1] > $panes[$i])
+            $increaseCount++;
+    }
+    echo "$increaseCount";
+}
--- day2.php
+++ day2.php
@@ -3,20 +3,42 @@
  * Author: Jared Scarito
  * Created: 12/3/2021 4:20 PM
  * /**/
-$input = fopen(__DIR__ . "/inputs/day2.txt", 'r') or die("Unable to open file...");
-$data = ['horizontal' => 0, 'depth' => 0];
-while ( ($line = fgets($input, 2024)) !== false) {
-    $exp = explode(" ", $line);
-    switch ($exp[0]) {
-        case "up":
-            $data['depth'] -= intval($exp[1]);
-            break;
-        case "down":
-            $data['depth'] += intval($exp[1]);
-            break;
-        case "forward":
-            $data['horizontal'] += intval($exp[1]);
-            break;
+function part1() {
+    $input = fopen(__DIR__ . "/inputs/day2.txt", 'r') or die("Unable to open file...");
+    $data = ['horizontal' => 0, 'depth' => 0];
+    while (($line = fgets($input, 2024)) !== false) {
+        $exp = explode(" ", $line);
+        switch ($exp[0]) {
+            case "up":
+                $data['depth'] -= intval($exp[1]);
+                break;
+            case "down":
+                $data['depth'] += intval($exp[1]);
+                break;
+            case "forward":
+                $data['horizontal'] += intval($exp[1]);
+                break;
+        }
     }
+    echo $data['horizontal'] * $data['depth'];
 }
-echo $data['horizontal'] * $data['depth'];
+function part2() {
+    $input = fopen(__DIR__ . "/inputs/day2.txt", 'r') or die("Unable to open file...");
+    $data = ['horizontal' => 0, 'depth' => 0, 'aim' => 0];
+    while (($line = fgets($input, 2024)) !== false) {
+        $exp = explode(" ", $line);
+        switch ($exp[0]) {
+            case "up":
+                $data['aim'] -= intval($exp[1]);
+                break;
+            case "down":
+                $data['aim'] += intval($exp[1]);
+                break;
+            case "forward":
+                $data['horizontal'] += intval($exp[1]);
+                $data['depth'] += ($data['aim'] * intval($exp[1]));
+                break;
+        }
+    }
+    echo $data['horizontal'] * $data['depth'];
+}
