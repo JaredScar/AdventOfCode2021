de70b0d371465f8489754334c2f75cc293666c14
Jared <TheWolfBadger@gmail.com>
2021-12-08 15:12:49 -0500

    + Get ready for day8 + Finish README for day8 part 1

---
+++ PHP/day8.php
@@ -0,0 +1,18 @@
+<?php
+function part1() {
+    $input = fopen(__DIR__ . "/../inputs/day8_test.txt", 'r') or die("Unable to open file...");
+    while (($line = fgets($input, 2024)) !== false) {
+        //
+    }
+}
+function part2() {
+    $input = fopen(__DIR__ . "/../inputs/day8_test.txt", 'r') or die("Unable to open file...");
+    while (($line = fgets($input, 2024)) !== false) {
+        //
+    }
+}
+echo "<h1>Part 1</h1><pre>";
+echo part1();
+echo "</pre><h2>Part 2</h2><pre>";
+echo part2();
+echo "</pre>";
